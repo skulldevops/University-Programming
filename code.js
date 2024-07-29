a8bcec75b3f77496ae0ff66c909941c934a89121
Nivea Rose Parker <145732598+skulldevops@users.noreply.github.com>
2024-07-28 20:09:36 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+function updateText(){
+   document.getElementById("game").innerHTML = "The Mathster Says...";
+   document.getElementById("question").innerHTML = "";
+   document.getElementById("correct").innerHTML = "End of Game!";
+   document.getElementById("true").innerHTML = "Correct!",
+   document.getElementById("false").innerHTML = "Incorrect!";
+   document.getElementById("reset").innerHTML = "Play Again?";
+   document.getElementById("next").innerHTML = ".";
+}
