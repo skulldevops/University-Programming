de5ad9513dee5ab28d157a59945134ade5e33a51
Rose Parker <145732598+skulldevops@users.noreply.github.com>
2024-08-23 11:47:01 -0400

    Delete stringcodedump.js

--- stringcodedump.js
+++
@@ -1,8 +0,0 @@
-function updateText(){
-    document.getElementById("bs").innerHTML = 
-    "It's a Palidrome!";
-    }
-    const button = document.getElementById("check");
-    
-    // Disable the button
-    button.disabled = true;
